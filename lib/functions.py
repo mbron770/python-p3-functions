7c07be161ecc9d9fa41dc9dfd3a3c64cc2f023af
mbron770 <mbronfin.tsd@gmail.com>
2023-07-02 21:56:05 -0400

    edit functions

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,19 @@
 #!/usr/bin/env python3
 
-def greet_programmer():
-    pass
+def greet_programmer(greeting = 'hello programmer'):
+    print("Hello, programmer!")
+    #pass
 
 def greet(name):
-    pass
+    print(f"Hello, {name}!")
+    #pass
 
 def greet_with_default(name="programmer"):
-    pass
+    print(f"Hello, {name}!")
+    #pass
 
 def add(num1, num2):
-    pass
+    return num1 + num2
 
 def halve(number):
-    pass
+    return number / 2
